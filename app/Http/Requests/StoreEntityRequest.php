3565e907fdfc17ef9ff554abe65902c0a0111d04
CaddyDz <caddydz4@gmail.com>
2020-02-14 22:15:11 +0100

    Store entity request validation DTO
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ app/Http/Requests/StoreEntityRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEntityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'bail|required|string|max:100',
+            // Make sure the submitted "parent_id" if any, exists in DB
+            'parent_id' => 'nullable|integer|exists:entities,id',
+            'barcode' => 'nullable|digits_between:10,100|unique:entities',
+            'description' => 'bail|required|string|min:3|max:1000'
+        ];
+    }
+}
