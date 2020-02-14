7678a43720656514dc71d9553f2b30718ecbb45c
CaddyDz <caddydz4@gmail.com>
2020-02-14 22:26:45 +0100

    Entities wrapper component and a base tree
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ resources/js/components/EntityComponent.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<create-entity></create-entity>
+		<tree></tree>
+	</div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		axios.get("/api/entities").then(response => {
+			this.$store.commit("initEntities", response.data);
+		});
+	}
+};
+</script>
---
+++ resources/js/components/Tree.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<p v-for="(entity, index) in entities" :key="index">{{ entity.name }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	computed: {
+		entities() {
+			return this.$store.state.entities;
+		}
+	}
+};
+</script>
