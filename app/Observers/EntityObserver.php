00d520f3e6283d511bac7c49b413ccff27aebc26
CaddyDz <caddydz4@gmail.com>
2020-02-15 03:56:41 +0100

    More effecient method to set incrementing barcode
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Observers/EntityObserver.php
+++ app/Observers/EntityObserver.php
@@ -9,7 +9,7 @@ class EntityObserver
     public function creating(Entity $entity)
     {
         if ($entity->barcode === null) { // If barcode is not defined by user
-            $barcode = optional(Entity::latest()->first())->barcode; // Auto increment from the last one
+            $barcode = Entity::max('barcode') ?? 0; // Auto increment from the max one
             $entity->barcode = $barcode ? $barcode + 1 : 1;
         }
     }
