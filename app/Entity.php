cca79fbea27d51d219455dbf7ba80b1b62e88727
CaddyDz <caddydz4@gmail.com>
2020-05-30 06:45:04 +0100

    Added docs to Entity model
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- app/Entity.php
+++ app/Entity.php
@@ -4,6 +4,31 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Entity
+ *
+ * @property int $id
+ * @property int|null $parent_id
+ * @property string $name
+ * @property int $barcode
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Entity[] $children
+ * @property-read int|null $children_count
+ * @property-read \App\Entity|null $parent
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereBarcode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Entity whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Entity extends Model
 {
     protected $fillable = [
